fa196c1283f5990bb7cc5360e4f6d3b18f64b414
ASHOKJAYA <bapathuashokreddy@gmail.com>
2023-02-09 09:57:58 +0530

    feature2 Commit

--- helo.java
+++ helo.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Devops"); 
+        System.out.println("Hello, World! Devops feature2"); 
     }
 }
